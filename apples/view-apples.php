2172bae8550cf2db22550953a56f6c56648316de
flakesbig <oketchs702@gmail.com>
2023-09-26 15:26:47 +0300

    UI

---
+++ apples/view-apples.php
@@ -0,0 +1,162 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+if(!isset($_SESSION['user_id']))
+{
+    header("Location: ../index.php");
+    exit();
+}
+?>
+
+<?php
+    require('../mysqli_connect.php'); //Connect to the db
+
+    $errors = array(); //Initialize an error array.
+
+      
+
+    if(empty($errors)) //If everything is OK.
+    {?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+                <title>View Apples</title>
+
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+                <link rel="icon" href="../images/apple.jpg" type="image/jpg">
+
+                <?php
+                   $path = __DIR__;
+                   require_once("../includes/external_file_links.php");
+                ?>
+        </head>
+
+        <body id="body" style="position: relative; min-height: 100%; top: 0px; background-color:rgb(26, 255, 26);">
+
+                <!-- Navigation -->
+             <div class="container-fluid sticky-top" style="background-color:rgb(4,38,84);">
+
+                <nav class="navbar navbar-expand-md text-light " role="navigation" id="main_navbar">
+
+                        <button class="navbar-toggler" type="button" style="color:rgb(236,132,17)" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            Menu
+                            <span class="navbar-toggler-icon">
+                                <i class="fa fa-bars" style="color:#fff; font-size:28px;"></i>
+                            </span>
+                       </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">                                                
+                                                        
+                            <li class="nav-item">
+                                <a class="nav-link" style="color:rgb(236,132,17);" href="../logout.php">Logout</a>
+                            </li>                        
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+                
+                <div class="container-fluid">
+                    <div class="row" style="padding-left: 0px;">
+                    <h2 class="text-center">These are the available apples</h2>
+                    <!--Center Column Section-->
+                    <div class="col-12 table-responsive">
+                        
+                        <p>
+                            <?php
+                            try
+                            {                                      
+                                $query = "SELECT * FROM apples";
+                                $q = mysqli_stmt_init($dbcon);
+                                mysqli_stmt_prepare($q, $query);
+                                
+                                // execute query
+                                mysqli_stmt_execute($q);
+                                $result = mysqli_stmt_get_result($q);
+
+                                if(mysqli_num_rows($result) >= 1)
+                                {
+                                                                            //Table header.
+                                        echo '<table class="table " style="font-size:20px; font-weight: 600;">
+                                        <tr>
+                                            <th scope="col">Apple ID</th>
+                                            <th scope="col">YOP</th>
+                                            <th scope="col">Breed</th>
+                                            <th scope="col">Row</th>
+                                            <th scope="col">Column</th>
+                                            <th scope="col">Longitude</th>
+                                            <th scope="col">Latitude</th>
+                                        </tr>';
+                                        //Fetch and print all the records:
+                                        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+                                        {
+                                            
+                                            //Remove special characters that might already be i table
+                                            //reduce the chance of XSS exploits                           
+                                            $apple_id = htmlspecialchars($row['apple_id'], ENT_QUOTES);
+                                            $yop = htmlspecialchars($row['yop'], ENT_QUOTES);
+                                            $breed = htmlspecialchars($row['breed'], ENT_QUOTES);
+                                            $m_row = htmlspecialchars($row['row'], ENT_QUOTES);
+                                            $col = htmlspecialchars($row['col'], ENT_QUOTES);
+                                            $longitude = htmlspecialchars($row['longitude'], ENT_QUOTES);
+                                            $latitude = htmlspecialchars($row['latitude'], ENT_QUOTES);
+                                            
+                                            echo '<tr>'.
+                                                   '<td>'. $apple_id . '</td>' .
+                                                   '<td>' . $yop . '</td>
+                                                    <td>' . $breed . '</td>
+                                                    <td>' . $m_row . '</td>
+                                                    <td>' .$col . '</td>                                                      
+                                                    <td>' . $longitude . '</td>
+                                                    <td>' . $latitude . '</td>  
+                                                  </tr>';
+                                        }
+                                        echo '</table>'; //Close the table so that it is ready for displaying.
+                                        mysqli_free_result($result); //Free up the resources.
+                                                                  
+
+                                    
+                                } 
+                                else
+                                {
+                                    echo htmlspecialchars('No apples available.', ENT_QUOTES);
+                                }                    
+                             
+                             
+                        }//end of try block
+                        catch(Exception $e) // We finally handle any problems here
+                        {
+                            print "An Exception occured. Message: " . $e->getMessage();
+                            echo htmlspecialchars('An error occurred! Please contact the administrator.', ENT_QUOTES);
+                        }
+                        catch(Error $e)
+                        {
+                            print "An Error occured. Message: " . $e->getMessage();
+                            echo htmlspecialchars('An error occurred! Please contact the administrator.', ENT_QUOTES);
+                        }
+                            ?>
+                        </p>
+                    </div>
+                </div>
+                </div> 
+
+        </body>
+
+        </html>
+    <?php
+    }
+    else
+    {
+        $errorstring = "Error! The following error(s) occured:<br>";
+        foreach($errors as $msg) //Print each error
+        {
+            $errorstring .= " - $msg<br>\n";
+        }
+        $errorstring .= "Please try again.<br>";
+        $internal_error = $errorstring . "</p>";
+        
+    }
+       
+?>
+
---
+++ register/apple/register-apple.php
@@ -0,0 +1,256 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+if(!isset($_SESSION['user_id']))
+{
+    header("Location: ../../index.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register Apple</title>
+        
+        <link rel="icon" href="../../images/apple.jpg" type="image/jpg">
+
+        <?php
+           $path = __DIR__;
+           require_once("../../includes/external_file_links.php");
+        ?>
+        
+        <noscript><h3 style="text-align:center">Your browser does not support JavaScript!<br>Enable JavaScript in your browser.</h3></noscript>             
+        
+    </head>
+    <body style="background-color:rgb(26, 255, 26)"> 
+
+         <!-- Navigation -->
+         <div class="container-fluid sticky-top" style="background-color:rgb(4,38,84);">
+
+            <nav class="navbar navbar-expand-md text-light " role="navigation" id="main_navbar">
+
+                    <button class="navbar-toggler" type="button" style="color:rgb(236,132,17)" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
+                        <span class="navbar-toggler-icon">
+                            <i class="fa fa-bars" style="color:#fff; font-size:28px;"></i>
+                        </span>
+                   </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">                                                  
+                        <li class="nav-item">
+                            <a class="nav-link" style="color:rgb(236,132,17);" href="../../apples/view-apples.php">View Apples</a>
+                        </li>                            
+                        <li class="nav-item">
+                            <a class="nav-link" style="color:rgb(236,132,17);" href="../../logout.php">Logout</a>
+                        </li>                        
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    
+        <!--Validate Input-->
+        <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            require('process-register-apple.php');
+        } // End of the main Submit conditional.
+        ?>
+    
+        <div class="container" style="background-color:rgb(4,38,84);border-radius:25px; margin-top:80px; margin-bottom:80px; max-width:400px;">
+            <div class="d-flex justify-content-center h-100">
+                <div>                                
+                    <div class="d-flex justify-content-center" style="margin-top:10px;margin-bottom:10px;">
+                        <img  class="rounded-circle" src="../../images/leafy.jpg" width="170px" height="150px" style="background-color:rgb(255,255,255);" alt="Logo">
+                    </div>
+                    <div class="d-flex justify-content-center" style="margin-top:10px;margin-bottom:10px;">
+                        <h2 style="color:rgb(236,132,17)">Register Apple</h2>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <form action="register-apple.php" method="post" name="appleform" id="appleform" style="display: inline-block;">                        
+                            <div class="row input-group mb-3">
+                                <div class="col-lg-4 input-group-append">
+                                    <span class="input-group-text" style="color:rgb(236,132,17);background-color:rgb(4,38,84); border:none;margin-right:10px;margin-bottom:5px;">Apple ID:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <input type="text" class="form-control" id="apple_id" name="apple_id" placeholder="Enter Apple ID"
+                                            maxlength="30" required 
+                                            value="<?php if(isset($_POST['apple_id'])) echo htmlspecialchars($_POST['apple_id'], ENT_QUOTES); ?>">
+                                </div>
+
+                            </div>
+
+                                                      
+
+                            <div class="row input-group mb-3">
+
+                                <div class="col-lg-4 input-group-append">
+                                    <span class="input-group-text" style="color:rgb(236,132,17);background-color:rgb(4,38,84); border:none;margin-right:10px;margin-bottom:5px;">YOP:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <input type="text" class="form-control" id="yop" name="yop" placeholder="Enter YOP"
+                                                maxlength="20" required                                                
+                                                value="<?php if(isset($_POST['yop'])) echo htmlspecialchars($_POST['yop'], ENT_QUOTES); ?>">
+                                                <i class="far fa-eye" id="togglePassword" style="margin-left: -30px; cursor: pointer; color:black;" ></i>
+                                </div>                 
+
+                            </div>
+
+                            <div class="row input-group mb-3">
+                                <div class="col-lg-4 input-group-append">
+                                    <span class="input-group-text" style="color:rgb(236,132,17);background-color:rgb(4,38,84); border:none;margin-right:10px;margin-bottom:5px;">Breed:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <select class="form-select" id="breed" name="breed" required>
+                                        <option value="" disabled selected>Select Breed</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            
+                            <div class="row input-group mb-3">
+
+                                <div class="col-lg-4 input-group-append">
+                                    <span class="input-group-text" style="color:rgb(236,132,17);background-color:rgb(4,38,84); border:none;margin-right:10px;margin-bottom:5px;">Row:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <input type="text" class="form-control" id="row" name="row" placeholder="Enter row"
+                                                maxlength="20" required
+                                                value="<?php if(isset($_POST['row'])) echo htmlspecialchars($_POST['row'], ENT_QUOTES); ?>">
+                                                
+                                </div>                 
+
+                            </div> 
+                            
+                             <div class="row input-group mb-3">
+
+                                <div class="col-lg-4 input-group-append">
+                                    <span class="input-group-text" style="color:rgb(236,132,17);background-color:rgb(4,38,84); border:none;margin-right:10px;margin-bottom:5px;">Column:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <input type="text" class="form-control" id="column" name="column" placeholder="Enter column"
+                                                maxlength="20" required
+                                                value="<?php if(isset($_POST['column'])) echo htmlspecialchars($_POST['column'], ENT_QUOTES); ?>">
+                                                
+                                </div>                 
+
+                            </div> 
+
+                             <div class="row input-group mb-3">
+
+                                <div class="col-lg-4 input-group-append">
+                                    <span class="input-group-text" style="color:rgb(236,132,17);background-color:rgb(4,38,84); border:none;margin-right:10px;margin-bottom:5px;">Geo-location:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span id="geolocation_data" style="color:white;"></span>
+                                    <input type="hidden" class="form-control" id="geolocation" name="geolocation" placeholder="longitude, latitude"
+                                               required
+                                                value="<?php if(isset($_POST['geolocation'])) echo htmlspecialchars($_POST['geolocation'], ENT_QUOTES); ?>">
+                                </div>                                               
+
+                            </div> 
+                            
+                            <div class="d-flex justify-content-center mt-3" style="margin-bottom:40px"> 
+                                <button class="btn btn-primary rounded-pill" type="button" style="width:200px;" onclick="getLocation()">
+                                    Update Location
+                                </button> 
+                            </div>
+
+                            <div class="text-center" style="color:red;">
+                                <?php 
+                                        if(isset($apple_id_exist)) 
+                                       {
+                                           echo $apple_id_exist;
+                                           $apple_id_exist = "";
+                                       }
+                                            
+                                ?>                               
+                            </div>
+                                                                                    
+                            <div class="text-center" style="color:red;">
+                                <?php 
+                                        if(isset($internal_error)) 
+                                       {
+                                           echo $internal_error;
+                                           $internal_error = "";
+                                       }
+                                            
+                                ?>                               
+                            </div>
+
+                            <div class="text-center" style="color:cyan;">
+                                <?php 
+                                        if(isset($success)) 
+                                        {            
+                                            echo  $success;
+                                            $success = "";
+                                        }
+                                            
+                                ?>                               
+                            </div>
+
+                            
+
+                            <div class="d-flex justify-content-center mt-3" style="margin-bottom:40px"> 
+                                <input id="submit" class="btn btn-primary rounded-pill" style="width:200px;" type="submit" name="submit" value="Register Apple" style="background-color:rgb(236,132,17);margin-bottom:5px;">            
+                            </div>            
+                                
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+       
+   
+
+        <script>
+            var loc = document.getElementById('geolocation_data');
+            var geolocation_details = "";
+            function getLocation()
+            {
+                if(navigator.geolocation)
+                    navigator.geolocation.getCurrentPosition(showPosition);
+                else
+                  {
+                    loc.innerHTML = "Geolocation is not supported by this browser.";
+                  }
+            }
+            function showPosition(position)
+            {
+                loc.innerHTML = "Latitude: " + position.coords.latitude.toFixed(8) + "<br>Longitude: " + position.coords.longitude.toFixed(8);
+                geolocation_details = position.coords.latitude + ',' + position.coords.longitude;
+
+                var geolocation_input_field = document.getElementById('geolocation');
+
+                geolocation_input_field.value = geolocation_details;
+            }
+            
+            getLocation();
+
+            
+            document.addEventListener("DOMContentLoaded", function () {
+                // Fetch breed options from the PHP script
+                fetch("./fetch-breeds.php")
+                    .then(response => response.json())
+                    .then(data => {
+                        const breedSelect = document.getElementById("breed");
+                        data.forEach(breed => {
+                            const option = document.createElement("option");
+                            option.value = breed;
+                            option.textContent = breed;
+                            breedSelect.appendChild(option);
+                        });
+                    })
+                    .catch(error => {
+                        console.error("Error fetching breed options: " + error);
+                    });
+            });
+
+
+
+            
+        </script>
+    </body>
+</html>
