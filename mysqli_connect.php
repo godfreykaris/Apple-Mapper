7d5929726b1e4119fcbfbdfdc2edfe8b51e845d5
flakesbig <oketchs702@gmail.com>
2023-09-27 14:04:27 +0300

    latest changes

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -4,7 +4,7 @@
 Define ('DB_USER', 'root'); //or whatever userid you created
 Define ('DB_PASSWORD', ''); //or whatever password you created
 Define ('DB_HOST', 'localhost');
-Define ('DB_NAME', 'applemapperdb');
+Define ('DB_NAME', 'applemapper');
 
 $dbcon = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
